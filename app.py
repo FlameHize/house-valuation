893bcd02abe5d71c7fa09f886aa2a845ac5067e5
zhuguangxu <zhuguangxu@njzq.com.cn>
2026-06-10 15:40:03 +0800

    feat: 添加计算理论和计算参数说明

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ st.set_page_config(page_title="房产估值工具", layout="wide")
 home = st.Page("pages/home.py", title="房产估值", icon="🏠", default=True)
 hedonic = st.Page("pages/1_hedonic.py", title="房源调整", icon="📐")
 prepay = st.Page("pages/2_prepayment.py", title="提前还贷", icon="💰")
+methodology = st.Page("pages/3_methodology.py", title="计算依据", icon="📖")
 
-pg = st.navigation([home, hedonic, prepay])
+pg = st.navigation([home, hedonic, prepay, methodology])
 pg.run()
